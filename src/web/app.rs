b766f28ae9bd67f57765402775b710eb9b674067
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-02-17 17:33:29 -0800

    Managed connections

--- src/web/app.rs
+++ src/web/app.rs
@@ -1,11 +1,24 @@
+
+use std;
+use diesel;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+use r2d2;
+use r2d2_diesel;
 use rocket::Rocket;
 use super::members;
 use VERSION;
 
 pub fn mount_app(rocket: Rocket) -> Rocket {
+    let env_var = std::env::var("DATABASE_URL").unwrap();
+    let manager = r2d2_diesel::ConnectionManager::<PgConnection>::new(env_var);
+    let pool = r2d2::Pool::new(manager).expect("Failed to create pool.");
+
     rocket
         .mount("/", routes![version])
-        .mount("/members", routes![members::hello])
+        .mount("/members", members::routes())
+        .manage(pool)
 }
 
 #[get("/version")]
